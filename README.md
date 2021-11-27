2a13267feedd611317fcd3b2c9b458855c45206e
sanghyunbak <sanghyunbak@gmail.com>
2021-11-28 08:00:50 +0900

    add markdown add .gitignore

---
+++ README.md
@@ -0,0 +1,14 @@
+# High Performance python
+
+-----
+#### 
+
+---
+
+```java
+public class BootSpringBootApplication {
+  public static void main(String[] args) {
+    System.out.println("Hello, Honeymon");
+  }
+}
+```
--- com/shbak/study/chapters/_01_understanding/main.py
+++ com/shbak/study/chapters/_01_understanding/main.py
@@ -13,6 +13,19 @@ def check_prime(number):
     return True
 
 
+def check_prime_vector(number):
+    sqrt_number = math.sqrt(number)
+    numbers = range(2, int(sqrt_number) + 1)
+    for i in range(0, len(numbers), 5):
+        result = (number / numbers[i:(i+5)]).is_integer()
+        if any(result):
+            return False
+    return True
+
 if __name__ == '__main__':
     print(colored(f'check_prime(10,000,000): {check_prime(10_000_000)}', 'green'))
     print(colored(f'check_prime(10,000,019): {check_prime(10_000_019)}', 'green'))
+
+    print(colored(f'check_prime(10,000,000): {check_prime_vector(10_000_000)}', 'green'))
+    print(colored(f'check_prime(10,000,019): {check_prime_vector(10_000_019)}', 'green'))
+
